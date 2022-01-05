c08b8b49bd21bf7b38eb4eecf995c1e31a7e2e1e
Brent Carey <69613439+brentcareycode@users.noreply.github.com>
2022-01-04 19:12:19 -0800

    Add responsiveness to modal

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import close from '../assets/close-24px.svg'
 import './Modal.css'
 
 const Modal = (props) => {
@@ -6,8 +7,9 @@ const Modal = (props) => {
     <div className='backdrop'>
       <section className='modal'>
         {/* TODO: Make this Dynamic */}
-        {props.title}
-        <p>{props.message}</p>
+        <img onClick={props.oncClick} className="modal__image" src={close} alt='close'/>
+        <h1 className='modal__title'>{props.title}</h1>
+        <p className='modal__message'>{props.message}</p>
       </section>
     </div>
   );
