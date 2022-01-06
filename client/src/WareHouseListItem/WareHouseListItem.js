f43742e5fe14549a529f6e561b6cdff29f858061
MIRIAM NAKI <nakiyingim@gmail.com>
2022-01-06 02:02:48 -0800

    add scss to warehouse list component

--- client/src/WareHouseListItem/WareHouseListItem.js
+++ client/src/WareHouseListItem/WareHouseListItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './WareHouseListItem.css';
+import './WareHouseListItem.scss';
 import chevronIcon from '../assets/chevron_right-24px.svg';
 import deleteIcon from '../assets/delete_outline-24px.svg';
 import editIcon from '../assets/edit-24px.svg';
@@ -9,35 +9,36 @@ const WareHouseListItem = (props) => {
   // const {wareHouseName, address, contactName, contactInfo} = props
   return (
     <>
-      <article className='ware-house-list-item'>
-      <div className='ware-house-list-item__wrapper'>
-        <div className='ware-house-list-item__right'>
-          <div className='ware-house-list-item__container'>
-            <p className='ware-house-list-item__name'>Manhattan</p>
-            <img className='ware-house-list-item__icon'src={chevronIcon} alt='chevron icon'/>
+    {/* ⇧⌘L */}
+      <article className='warehouse-list-item'>
+      <div className='warehouse-list-item__wrapper'>
+        <div className='warehouse-list-item__right'>
+          <div className='warehouse-list-item__container'>
+            <p className='warehouse-list-item__name'>Manhattan</p>
+            <img className='warehouse-list-item__icon'src={chevronIcon} alt='chevron icon'/>
           </div>
-          <p className='ware-house-list-item__address'>
+          <p className='warehouse-list-item__address'>
             503 Broadway
             New York, USA
           </p>
           
         </div>
 
-        <div className='ware-house-list-item__left'>
-          <p className='ware-house-list-item__contact-name'>Permin Aujla</p>
-          <p className='ware-house-list-item__contact-info'>
+        <div className='warehouse-list-item__left'>
+          <p className='warehouse-list-item__contact-name'>Permin Aujla</p>
+          <p className='warehouse-list-item__contact-info'>
           +1(629)555-0129
           paujla@instock.com
           </p>
-          <div className='ware-house-list-item__icons desktop'>
-            <img className='ware-house-list-item__delete' src={deleteIcon}/>
-            <img className='ware-house-list-item__edit'src={editIcon}/>
+          <div className='warehouse-list-item__icons warehouse-list-item__icons--tablet'>
+            <img className='warehouse-list-item__delete' src={deleteIcon} alt='delete icon'/>
+            <img className='warehouse-list-item__edit'src={editIcon} alt='edit icon'/>
         </div>    
       </div> 
       </div>     
-      <div className='ware-house-list-item__icons mobile'>
-        <img className='ware-house-list-item__delete' src={deleteIcon}/>
-        <img className='ware-house-list-item__edit'src={editIcon}/>
+      <div className='warehouse-list-item__icons warehouse-list-item__icons--mobile'>
+        <img className='warehouse-list-item__delete' src={deleteIcon} alt='delete icon '/>
+        <img className='warehouse-list-item__edit'src={editIcon} alt='edit icon'/>
       </div>
       </article>
       <hr/>
