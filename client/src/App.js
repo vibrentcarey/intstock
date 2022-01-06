52411d6c4a3069a706772b74d752271d2610ebaf
Brent Carey <69613439+brentcareycode@users.noreply.github.com>
2022-01-05 19:44:05 -0800

    Create details component for top bar

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import TopBar from './components/TopBar';
 function App() {
   return (
     <div className="App">
-      <TopBar title='King West' />
+      <TopBar title='King West' details={{address: '13 fun road', city: 'Van', country: 'CAN'}} />
 
     </div>
   );
