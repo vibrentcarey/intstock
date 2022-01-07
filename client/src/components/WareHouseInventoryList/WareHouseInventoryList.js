a51b7affc85ea09bc810718b3fb285859597405c
MIRIAM NAKI <nakiyingim@gmail.com>
2022-01-07 01:10:40 -0800

    add mobile stying

--- client/src/components/WareHouseInventoryList/WareHouseInventoryList.js
+++ client/src/components/WareHouseInventoryList/WareHouseInventoryList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './WareHouseInventoryList.scss'
 import chevronIcon from '../../assets/chevron_right-24px.svg';
 import deleteIcon from '../../assets/delete_outline-24px.svg';
 import editIcon from '../../assets/edit-24px.svg';
@@ -8,24 +9,27 @@ const WareHouseInventoryList = () => {
     <>
       <article className='inventory'>
         <div className='inventory__container'>
+
           <div className='inventory__item-category'>
-            <div className='inventory__name-wrapper'>
-              <p>Television</p>
-              <img src={chevronIcon} alt='arrow icon'/>
-            </div>  
-            <p>Electronics</p>
+            <div className='inventory__wrapper'>
+              <div className='inventory__name-wrapper'>
+                <p className='inventory__name inventory__item'>Television</p>
+                <img className='inventory__arrow'src={chevronIcon} alt='arrow icon'/>
+              </div>  
+            </div>
+            <p className='inventory__category inventory__item'>Electronics</p>
           </div>
 
           <div className='inventory__status-quantiy'>
-            <p>Status</p>
-            <p>500</p>
+            <p className='inventory__status inventory__item'>Status</p>
+            <p className='inventory__quantity inventory__item'>500</p>
           </div>
 
-          <div className='inventory__icons'>
-            <img src={deleteIcon} alt='delete icon'/>
-            <img src={editIcon} alt='edit icon'/>
           </div>
-        </div>
+          <div className='inventory__icons'>
+            <img className='inventory__icon' src={deleteIcon} alt='delete icon'/>
+            <img className='inventory__icon' src={editIcon} alt='edit icon'/>
+          </div>
       </article>
     </>
   )
