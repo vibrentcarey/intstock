a1e0cedb6572693474187a45c51b36f659da07ce
MIRIAM NAKI <nakiyingim@gmail.com>
2022-01-07 13:39:12 -0800

    add endpoint to fetch inventories for a given warehouse

--- server/routes/inventories.js
+++ server/routes/inventories.js
@@ -2,30 +2,11 @@ const {Router} = require('express');
 const inventoriesRouter = Router();
 const fs = require('fs');
 
-
-const readInventoriesData = () => {
-  const data = fs.readFileSync('./data/inventories.json');
-  return JSON.parse(data);
-}
-const readWarehouseData = () => {
-  const data = fs.readFileSync('./data/warehouses.json');
-  return JSON.parse(data);
-}
-
-
 // Fetch inventory list end point
 inventoriesRouter.get('/', (_req, res) => {
-  const inventoryData = readInventoriesData()
-  res.status(200).json(inventoryData)
   
 });
 inventoriesRouter.get('/:warehouseId', (req, res) => {
-  const inventoryData = readInventoriesData()
-  const warehouseData = readWarehouseData()
-  const wareHouseId = warehouseData.find(wareHouse => wareHouse.id === req.params.wareHouseId);
-  const inventories = inventoryData.find(inventory => inventory.wareHouseId === wareHouseId);
-
-  res.status(200).json(inventories)
   
 });
 
--- server/routes/warehouses.js
+++ server/routes/warehouses.js
@@ -3,19 +3,27 @@ const wareHouseRouter = Router();
 const fs = require('fs');
 
 
-const readWarehouseData = () => {
-  const data = fs.readFileSync('./data/warehouses.json');
+
+// read from file
+const readInventoriesData = () => {
+  const data = fs.readFileSync('./data/inventories.json');
   return JSON.parse(data);
 }
 // Fetch warehouse list end point
 wareHouseRouter.get('/', (_req, res) => {
-  const wareHouseData = readWarehouseData()
-  res.status(200).json(wareHouseData);
+ 
 });
 
 // Fetch a single warehouse
 wareHouseRouter.get('/:wareHouseId', (req, res) => {
   
+});
+
+// get each warehouse inventory details
+wareHouseRouter.get('/:wareHouseId/inventories', (req, res) => {
+  const inventoryData = readInventoriesData()
+  const wareHouses = inventoryData.filter(inv => inv.warehouseID === req.params.wareHouseId)
+  res.status(200).json(wareHouses); 
 });
 
 //create warehouse
