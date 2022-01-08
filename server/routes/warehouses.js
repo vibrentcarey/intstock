3817fe07c9806901fae656d19a462d8b58c727b3
Brent Carey <69613439+brentcareycode@users.noreply.github.com>
2022-01-07 17:23:25 -0800

    Add route to add warehouse

--- server/routes/warehouses.js
+++ server/routes/warehouses.js
@@ -1,4 +1,4 @@
-const {Router} = require('express');
+const { Router } = require('express');
 const wareHouseRouter = Router();
 const express = require("express");
 const fs = require("fs");
@@ -33,33 +33,42 @@ const readInventoriesData = () => {
 }
 // Fetch warehouse list end point
 wareHouseRouter.get('/', (_req, res) => {
- 
+
 });
 
 // Fetch a single warehouse
 wareHouseRouter.get('/:wareHouseId', (req, res) => {
-  
+
 });
 
 // get each warehouse inventory details
 wareHouseRouter.get('/:wareHouseId/inventories', (req, res) => {
   const inventoryData = readInventoriesData()
   const wareHouses = inventoryData.filter(inv => inv.warehouseID === req.params.wareHouseId)
-  res.status(200).json(wareHouses); 
+  res.status(200).json(wareHouses);
 });
 
 //create warehouse
-wareHouseRouter.post('/',(req, res) => {
-
+wareHouseRouter.post('/', (req, res) => {
+  const warehouses = readFile();
+  // Validate request details
+  if (!req.body || !req.body.name || !req.body.address || !req.body.city || !req.body.country || !req.body.contact) {
+    // Send back error message
+    return res.status(400).json({ message: 'Please send required fields' });
+  }
+  // Add new warehouse to file
+  warehouses.push(req.body);
+  fs.writeFileSync('./data/warehouses.json', JSON.stringify(warehouses));
+  res.status(200).json({ message: 'Successfully Created Warehouse' });
 });
 
 // edit a warehouse
 wareHouseRouter.patch('/:wareHouseId', (req, res) => {
- 
+
 });
 
 // delete a warehouse
-wareHouseRouter.delete('/:wareHouseId', (req,res) => {
+wareHouseRouter.delete('/:wareHouseId', (req, res) => {
 
 })
 
