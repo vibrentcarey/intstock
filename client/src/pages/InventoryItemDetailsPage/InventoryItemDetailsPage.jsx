878c938f914d6c12fe0400735922cabe683d42db
jodrasko <youremail@provider.com>
2022-01-08 20:54:01 -0800

    Complete Front-End: Inventory Item Details Component (Responsive)

--- client/src/pages/InventoryItemDetailsPage/InventoryItemDetailsPage.jsx
+++ client/src/pages/InventoryItemDetailsPage/InventoryItemDetailsPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import { Redirect } from "react-router";
 import { Link } from "react-router-dom";
 import Button from "../../Button/Button";
@@ -6,34 +7,47 @@ import LabelValue from "../../components/LabelValue/LabelValue";
 import back from "../../assets/arrow_back-24px.svg";
 import "../InventoryItemDetailsPage/InventoryItemDetailsPage.scss";
 
-const dummyInventoryItem = {
-  id: "9b4f79ea-0e6c-4e59-8e05-afd933d0b3d3",
-  warehouseID: "2922c286-16cd-4d43-ab98-c79f698aeab0",
-  warehouseName: "Manhattan",
-  itemName: "Television",
-  description:
-    'This 50", 4K LED TV provides a crystal-clear picture and vivid colors.',
-  category: "Electronics",
-  status: "In Stock",
-  quantity: 500
-};
-
 // Inventory Item Details Page
 class InventoryItemDetailsPage extends React.Component {
   state = {
-    redirectToHome: false,
-    details: dummyInventoryItem
+    redirectToEdit: false,
+    details: null
   };
+
+  getInventoryItemDetails = (id) => {
+    const url = `http://localhost:8080/inventories/${id}`;
+
+    axios
+      .get(url)
+      .then((res) => {
+        this.setState({
+          details: res.data
+        });
+      })
+      .catch((err) => {
+        console.log("error in axios", err);
+      });
+  };
+
+  // getting inventory item details from api upon website mount
+  componentDidMount() {
+    // invoking getInventoryItemDetails function to get all inventory item details
+    this.getInventoryItemDetails(this.props.match.params.inventoryItemId);
+  }
 
   handleEditButtonClick = () => {
     this.setState({
-      redirectToHome: true
+      redirectToEdit: true
     });
   };
 
   render() {
-    if (this.state.redirectToHome) {
-      return <Redirect to={`/edit-inventory-item/${this.state.details.id}`} />;
+    if (this.state.redirectToEdit) {
+      return (
+        <Redirect
+          to={`/edit-inventory-item/${this.props.match.params.inventoryItemId}`}
+        />
+      );
     }
 
     return (
@@ -60,34 +74,39 @@ class InventoryItemDetailsPage extends React.Component {
             </div>
           </section>
         </div>
-
-        <section className="inventory-item-details__bottom-section">
-          <div className="inventory-item-details__container-left">
-            <LabelValue
-              label="ITEM DESCRIPTION"
-              value={this.state.details.description}
-            />
-            <LabelValue label="CATEGORY" value={this.state.details.category} />
-          </div>
-
-          <div className="inventory-item-details__container-right">
-            <div className="inventory-item-details__status">
+        {this.state.details && (
+          <section className="inventory-item-details__bottom-section">
+            <div className="inventory-item-details__container-left">
               <LabelValue
-                label="STATUS"
-                value={this.state.details.status}
-                type="stock"
+                label="ITEM DESCRIPTION"
+                value={this.state.details.description}
               />
               <LabelValue
-                label="QUANTITY"
-                value={this.state.details.quantity}
+                label="CATEGORY"
+                value={this.state.details.category}
               />
             </div>
-            <LabelValue
-              label="WAREHOUSE"
-              value={this.state.details.warehouseName}
-            />
-          </div>
-        </section>
+
+            <div className="inventory-item-details__container-right">
+              <div className="inventory-item-details__status">
+                <LabelValue
+                  label="STATUS"
+                  value={this.state.details.status}
+                  type="stock"
+                />
+                <LabelValue
+                  label="QUANTITY"
+                  value={this.state.details.quantity}
+                />
+              </div>
+              <LabelValue
+                label="WAREHOUSE"
+                value={this.state.details.warehouseName}
+              />
+            </div>
+          </section>
+        )}
+        ;
       </main>
     );
   }
