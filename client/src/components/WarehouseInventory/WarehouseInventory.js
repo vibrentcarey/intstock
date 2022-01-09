45b41692f26fefae7c015d4fd26c8ce58b4a84b7
MIRIAM NAKI <nakiyingim@gmail.com>
2022-01-08 21:22:24 -0800

    add warehouse inventory list

--- client/src/components/WarehouseInventory/WarehouseInventory.js
+++ client/src/components/WarehouseInventory/WarehouseInventory.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import InventoryItem from '../InventoryItem/InventoryItem';
 
 
-const WarehouseInventoryList = () => {
+const WarehouseInventory = (props) => {
+  // props passed from Warehouse Details page
+  const {inventoryList} = props
   return (
-    <div>
-      
-    </div>
+    <>
+    {/* map over inventory list */}
+    {inventoryList.map(inventory => { 
+      return (
+        <InventoryItem 
+          key={inventory.id}
+          name={inventory.itemName}
+          category={inventory.category}
+          quantity={inventory.quantity}
+          />
+      )     
+    })}
+    </>
   )
 }
 
-export default WarehouseInventoryList
+export default WarehouseInventory
 
--- client/src/pages/WarehouseDetailsPage/WarehouseDetailsPage.jsx
+++ client/src/pages/WarehouseDetailsPage/WarehouseDetailsPage.jsx
@@ -1,5 +1,6 @@
 import React,{Component} from "react";
 import TopBar from '../../components/TopBar';
+import { withRouter } from "react-router-dom";
 import WarehouseInventory from "../../components/WarehouseInventory/WarehouseInventory";
 import TopBarDetails from "../../components/TopBarDetails";
 import CategoryBar from '../../components/CategoryBar';
@@ -11,17 +12,29 @@ class WarehouseDetailsPage extends Component {
   state = {
     inventories:[]
   }
-  
-  componentDidMount(){
-    const id = '2922c286-16cd-4d43-ab98-c79f698aeab0'
-    axios.get(`http://localhost:8080/warehouses/${id}/inventories`)
+
+  // pass warehouse id to endpoint url
+  fetchInventories = (warehouseId) => {
+    axios.get(`http://localhost:8080/warehouses/${warehouseId}/inventories`)
     .then(res => {
       console.log(res.data)
       this.setState({inventories: res.data})
-    })  
+    }) 
+    .catch(err => {
+      return err;
+    }) 
+  }
+
+  // initial inventory list fetch
+  componentDidMount(){
+    // Please check router props!!!!
+    let warehouseId = '2922c286-16cd-4d43-ab98-c79f698aeab0'
+    this.fetchInventories(warehouseId)
   }
   
+
   render(){
+    console.log(this.props)
     return (
       <>
         <CategoryBar categories={['inventory', 'category','status','quantity','actions']}/>
@@ -30,7 +43,6 @@ class WarehouseDetailsPage extends Component {
       </>
     );
   };
-
   }
   
-export default WarehouseDetailsPage;
+export default withRouter(WarehouseDetailsPage);
